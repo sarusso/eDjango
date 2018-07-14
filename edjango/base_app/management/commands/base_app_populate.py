db2634b0390a81fe8f63b772dd6f8d99325a424b
Stefano Alberto Russo <stefano.russo@gmail.com>
2018-07-14 13:18:07 +0200

    Fixed bug in base app populate (admin user creation).

--- edjango/base_app/management/commands/base_app_populate.py
+++ edjango/base_app/management/commands/base_app_populate.py
@@ -1,11 +1,11 @@
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.conf import settings
 
-class Command(NoArgsCommand):
+class Command(BaseCommand):
 
-    def handle_noargs(self, *args, **kwargs):
- 
+    def handle(self, *args, **kwargs):
+
         if not User.objects.filter(username='admin').exists():
             print 'Creating default admin: user=admin, pass=admin. Change it!!'
             admin = User.objects.create_superuser('admin', 'admin@edjango.local', 'admin')
